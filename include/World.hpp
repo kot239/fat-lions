258cad20d0a1f2d1da988d9b74ecaf22a5f1011f
NikitaAbramov <nik_abramov22@mail.ru>
2020-04-24 19:19:30 +0300

    fix another bug

--- include/World.hpp
+++ include/World.hpp
@@ -11,10 +11,10 @@
 
 namespace world {
 
-const int AGE_FOR_REPROD = 5;
+const int AGE_FOR_REPROD = 100;
 const int MAX_AGE = 100000000;
 const int HUNGER_FOR_REPROD = 10;
-const int CD_REPROD_TIME = 3;
+const int CD_REPROD_TIME = 100;
 const int MAX_HUNGER = 100000000;
 const int GRASS_NUTRITION = 10;
 const double START_VELOCITY = 5;
